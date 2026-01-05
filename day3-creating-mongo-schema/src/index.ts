756ddf47ea55cda282e9801818c8f2c6ea91765d
strngpowty <strngpowty@gmail.com>
2026-01-05 23:08:58 +0530

    Day3 Mongo DB connected with Express Application

--- day3-creating-mongo-schema/src/index.ts
+++ day3-creating-mongo-schema/src/index.ts
@@ -1,11 +1,16 @@
-import express from 'express'
-import 'dotenv/config'
-import type { Express } from 'express'
+import express from "express";
+import "dotenv/config";
+import type { Express } from "express";
+import connectDB from "./config/db.js";
 
+const app: Express = express();
+const PORT = Number(process.env.PORT);
 
-const app : Express = express()
-const PORT = Number(process.env.PORT)
+const startServer = async () => {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log("Server Running at ", PORT);
+  });
+};
 
-app.listen(PORT, () => {
-    console.log("Server Running at ", PORT)
-})
+startServer()
