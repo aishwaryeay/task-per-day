947edf9f676087e19e1f612442748097f6eb4e69
strngpowty <strngpowty@gmail.com>
2026-01-06 06:06:03 +0530

    Day3 creating of mongodb schema completed

---
+++ day3-creating-mongo-schema/src/models/User.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
+    name : string
+    email: string
+    password: string
+    createAt: Date
+
+}
+
+const UserSchema : Schema = new Schema (
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        }, 
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: 6
+        }
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+
+export default mongoose.model<IUser>("User", UserSchema)
