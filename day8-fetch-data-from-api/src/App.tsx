e1d16dcdf4cf03c624442625fd7fc2d95a16b49e
strngpowty <strngpowty@gmail.com>
2026-01-11 21:42:32 +0530

    Implemented suggested changes

--- day8-fetch-data-from-api/src/App.tsx
+++ day8-fetch-data-from-api/src/App.tsx
@@ -1,42 +1,40 @@
-import { useState, useEffect } from 'react'
-import  axios  from 'axios'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-const PRODUCTS_URL = "https://dummyjson.com/products"
+const PRODUCTS_URL = "https://dummyjson.com/products";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(true);
+  console.log(products);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const data = await axios.get(PRODUCTS_URL);
+        setProducts(data.data.products);
+      } catch (err) {
+        
+        setError("Whoops!! Something Went Wrong Please Try Again Later");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
 
-  const [ products, setProducts ] = useState([])
-  const [ error, setError ] = useState("")
-  const [ isLoading, setLoading] = useState(true)
-console.log(products)
-  useEffect(() =>{
-    const fetchProducts = async() => {
-   try {
-    const data = await axios.get(PRODUCTS_URL) 
-    
-    setLoading(false)
-    setProducts(data.data.products)
-   } catch (err) {
-    setLoading(false)
-    setError("Whoops!! Something Went Wrong Please Try Again Later")
-   }
-  }
-  fetchProducts()
-  }, [])  
-  
   return (
     <>
-    {isLoading && <p> Loading</p>}
-    {error && <p> {error} </p>}
-    {products && <div>
-
-      {products.map(product=>   <li key={product.id}>{product.title}</li>)}
-      
-      </div>}
-      
+      {isLoading && <p> Loading</p>}
+      {error && <p> {error} </p>}
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
