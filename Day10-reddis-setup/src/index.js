7d08ed70500d4c4a155d16b797f6920c4b9ddffb
strngpowty <strngpowty@gmail.com>
2026-01-18 22:38:47 +0530

    configuring reddis setup

--- Day10-reddis-setup/src/index.js
+++ Day10-reddis-setup/src/index.js
@@ -1,1 +1,22 @@
-import { express } from 'express'
+const express = require('express');
+const session = require('express-session');
+const redisClient = require('./redis');
+
+const app = express();
+
+app.use(
+  session({
+    store: new (require('connect-redis')(session))({
+      client: redisClient,
+    }),
+    secret: 'long-random-key', 
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, 
+  })
+);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
